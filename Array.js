a74f89761017da9e48240abe642f317e4544a3ca
DESKTOP-OO5GB7M\roman <romana.93@live.com>
2022-10-20 13:23:08 +0200

    novi komit

---
+++ Array.js
@@ -0,0 +1,40 @@
+// 1.zadatak
+
+var Array=[ "Januar","Februar","Mart","April","Maj","Jun","Jul","Avgust","Septembar","Oktobar","Novembar","Decembar"]
+console.log(Array[0]);
+console.log(Array[5]);
+console.log(Array[9]);
+
+// // 2.zadatak
+
+var Array=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday" ]
+console.log (Array[6]);
+
+// 3.zadatak
+
+var Array=[2, -4, 5, -2, -11]
+console.log(Array[1])
+console.log(Array[3])
+console.log(Array[4])
+
+// 4 zadatak
+
+ var Array=[5, 15, -5, 20, 12, 18,72, 14, 9]
+ console.log(Array[1])
+ console.log(Array[4])
+ console.log(Array[5])
+ console.log(Array[6])
+ console.log(Array[8])
+
+//  5 zadatak
+
+var Array=[[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]]
+
+console.log(Array.length)
+
+// **index br 24**
+console.log(Array[0][3])  
+
+console.log(Array[3])
+console.log(Array[2][3])
+
